0b6fbe4eea57cb17847f6c6fb4ecc8e849bacd00
Noel Parisi <NoelParisi@calvisitor-10-105-176-84.calvisitor.1918.berkeley.edu>
2017-03-07 20:31:03 -0800

    populates dom

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,52 @@
 // create an array of strings, each one related to a topic that interests you. 
 // Save it to a variable called topics.
-var topics = ['dancing horses', 'crazed women', 'jumping jupiter', 'dogs'];
+var topics = ['cats', 'sandwiches', 'crabs', 'dogs', 'spongebob', 'crabs', 'Obama', 'Joe Biden', 'bromance', 'baby elephants'];
 var searchQuery;
 var queryURL;
+var newSearch;
 // research q, limit, rating
-console.log(queryURL);
 
 var renderButtons = function() {
 
-    for (let i = 0; i < topics.length; i++) {
+    for (let j = 0; j < topics.length; j++) {
 
-        $('#gifButtons').append('<button> ' + topics[i] + ' </button>');
+        $('#gifButtons').append('<button>' + topics[j] + '</button>');
     }
 }
+
+$('#add-movie').on('click', function(event) {
+
+	$('#gifButtons').empty();
+
+	event.preventDefault();
+
+	newSearch = $('#movie-input').val();
+
+	topics.push(newSearch);
+
+	console.log(newSearch);
+
+	console.log(topics);
+
+	renderButtons();
+
+	$('input:text').val('');
+})
 
 
 renderButtons();
 
-if ($('#gifButtons').on('click', function(event) {
+
+
+$('#gifButtons').on('click', function(event) {
+
+	$('.gif').empty();
 
     event.preventDefault();
 
     searchQuery = $(event.target).text();
 
-    console.log(queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&limit=25&rating=g&api_key=dc6zaTOxFJmzC");
+    console.log(queryURL = "http://api.giphy.com/v1/gifs/search?q="+searchQuery+"&limit=10&rating=g&api_key=dc6zaTOxFJmzC");
 
     console.log(searchQuery);
 
@@ -31,16 +54,23 @@ if ($('#gifButtons').on('click', function(event) {
         url: queryURL,
         method: "GET"
     }).done(function(response) {
-    	videoTag = response.data[0].embed_url;
+
+    	console.log(response.data);
+
+    	for (var i=0; i < 10; i++){
+
+    		imgTag = response.data[i].images.original.url;
+
+    		ratingTag = response.data[i].rating;
+
+    		$('.gif').append('<p>Rating:'+ratingTag+'</p>'+'<img src='+imgTag+'>');
+    	}
         console.log(response);
-        console.log(response.data[0].rating);
-        console.log(videoTag);
+        // data > i > images > original > url 
 
-        // $('.gif').html();
+        // same as above but original_still for the still
     })
-}));
-
-
+});
 
 // Your app should take the topics in this array and create buttons in your HTML.
 // Try using a loop that appends a button for each string in the array.
